465763eadac38ca24571cb6650a6d7b69c954f14
Azzarox <62171309+Azzarox@users.noreply.github.com>
2026-01-16 17:54:14 +0200

    Added redirect to docs

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,33 @@
 
-import Koa from 'koa';
+import Koa, { Context } from 'koa';
 import json from 'koa-json';
-import { router } from './routes/router';
+import Router from '@koa/router';
+import { mainRouter } from './routes/router';
 import { bodyParser } from '@koa/bodyparser';
 import passport from './config/authConfig';
 import koaPinoLogger from 'koa-pino-logger';
 import logger from './config/logger';
 import { swaggerDocsRouter } from './config/swagger';
 
+const router = new Router();
+
+router.get('/', (ctx: Context) => ctx.redirect('/v1/docs'))
+router.get('/ping', (ctx: Context) => {
+    ctx.status = 200;
+    ctx.body = {
+        status: ctx.status,
+        message: 'success'
+    }
+})
+
 const app = new Koa();
-    
 app.use(koaPinoLogger({logger}));
 app.use(json());
 app.use(bodyParser());
 app.use(passport.initialize());
+app.use(router.routes());
 app.use(swaggerDocsRouter.routes());
-app.use(router.routes()).use(router.allowedMethods());
+app.use(mainRouter.routes()).use(mainRouter.allowedMethods());
 
 export default app;
 
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -4,10 +4,10 @@ import { booksRouter } from './books/booksRouter';
 import { userRouter } from './users/usersRouter';
 
 // Main router;
-export const router = new Router({
+export const mainRouter = new Router({
     'prefix': '/v1'
 });
 
-router.use(authRouter.routes()).use(authRouter.allowedMethods());
-router.use(booksRouter.routes()).use(booksRouter.allowedMethods());
-router.use(userRouter.routes()).use(userRouter.allowedMethods());
+mainRouter.use(authRouter.routes()).use(authRouter.allowedMethods());
+mainRouter.use(booksRouter.routes()).use(booksRouter.allowedMethods());
+mainRouter.use(userRouter.routes()).use(userRouter.allowedMethods());
